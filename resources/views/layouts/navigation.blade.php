d37378ab5baa29df5aa6aae84e370bbf1646fdb3
Miguel Figueroa <miguelfigueroa90@gmail.com>
2023-08-17 03:11:53 -0400

    Updating mobile menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -96,6 +96,18 @@
             </x-responsive-nav-link>
         </div>
 
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('receipts.index')" :active="request()->routeIs('receipts.index')">
+                {{ __('Receipts') }}
+            </x-responsive-nav-link>
+        </div>
+
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('ingredients.index')" :active="request()->routeIs('ingredients.index')">
+                {{ __('Ingredients') }}
+            </x-responsive-nav-link>
+        </div>
+
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="px-4">
